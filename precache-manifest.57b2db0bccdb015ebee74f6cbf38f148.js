66ddc67739381ee0064e8cb2b2268e692f3b0a0d
Maria Bich Pham <bntp.aabc@gmail.com>
2020-06-01 14:02:38 +0200

    Updates

---
+++ precache-manifest.57b2db0bccdb015ebee74f6cbf38f148.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1aa72a93069613226c6ba465c71d18d3",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "cb7fed3d3aad87c4c799",
+    "url": "/monsters-rolodex/static/css/main.b4a419eb.chunk.css"
+  },
+  {
+    "revision": "12240f73676d8f2fbd85",
+    "url": "/monsters-rolodex/static/js/2.a3c82928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a3c82928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb7fed3d3aad87c4c799",
+    "url": "/monsters-rolodex/static/js/main.9fda23dc.chunk.js"
+  },
+  {
+    "revision": "6062e4a584c29c6e0372",
+    "url": "/monsters-rolodex/static/js/runtime-main.e3d252a9.js"
+  }
+]);
